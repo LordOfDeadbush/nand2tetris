b62b82ff4fa2ace1ba39d903587ab8c7c8a5cdab
LordOfDeadbush <37131666+LordOfDeadbush@users.noreply.github.com>
2023-05-17 21:53:17 -0700

    made some notes on mult

--- projects/04/mult/Mult.asm
+++ projects/04/mult/Mult.asm
@@ -10,3 +10,23 @@
 // R0 >= 0, R1 >= 0, and R0*R1 < 32768.
 
 // Put your code here.
+
+// first we need to create our variables
+
+// @R0 would be R0, @R1 would be R1, @R2 would be R2
+// we want to multiply @0 by @1 to get @2
+// to do this, we need to make a loop with a counter that will break once it reaches @1, 
+// in which we will have a variable that will be increased by @0 every loop iteration
+
+
+// for reference, how to add 2 numbers:
+
+// @0
+// D=M
+// @1
+// D=D+M
+// @2
+// M=D
+
+// M is what is stored in the selected memory
+// D is our variable
